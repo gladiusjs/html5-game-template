2afcd094b7fdd36f08e2284160fc2869c8ec21c5
Dan Mosedale <dmose@meer.net>
2012-05-03 16:10:25 -0700

    Test that GameShim has installed itself after requiring it.

--- test/template/hello-game-world.js
+++ test/template/hello-game-world.js
@@ -4,7 +4,12 @@ module("basic functionality provided by the game template");
 asyncTest("installing game-shim", function() {
 
   expect(1);
-
+  
+  // Because GameShim installs itself in the window global,
+  // and there's no real way to uninstall it without writing the inverse
+  // code.  So all subsequent tests are going to run in this modified
+  // environment.  Yuck.
+  //
   require(['../www/js/lib/game-shim'], function () {
     ok('GameShim' in window, 
        'GameShim property in global object after requiring');
