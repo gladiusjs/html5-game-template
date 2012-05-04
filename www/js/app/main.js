8dd222f38df4b2dd57cfd4643da7bebfc94b54a8
Dan Mosedale <dmose@meer.net>
2012-05-04 14:19:15 -0700

    Wire up game-shim by default

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -1,4 +1,9 @@
-// Defines the main app module. This one does the top level app wiring.
-
-define(function (require) {
+// This file hosts the main game module.  Whenever it makes sense to
+// put something in its own module, you can either put it in the dependency
+// array for this module (i.e. in the array containing 'game-shim'), or you
+// can call require() directly closer to the time you need it.  
+// <http://requirejs.org/> has detail documentation.  
+//
+define(['game-shim'], function (require) {
+  
 });
