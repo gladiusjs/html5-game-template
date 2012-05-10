373bd6016e5f486de84c5f9268c707adf93d00e5
Dan Mosedale <dmose@meer.net>
2012-05-09 17:30:45 -0700

    Fix volofile to build and optimize libraries loaded by static script tags and workaround r.js bug that attempts to read commented-out modules, r=paired with jrburke

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -5,17 +5,18 @@
 // time you need it.  <http://requirejs.org/> has detailed documentation.  
 //
 //
-// Toji's game-shim module is included with this template; if you wish to use
-// it, replace the define() below this comment with this one:
-//
-// define(['game-shim'], function (gameshim) {
-//
-// The reason that this isn't done by default is that the replacement
-// versions of the various APIs that it includes could potentially cause
-// complications or performance issues with other libraries that use these APIs
-// and may have their own strategies for dealing with cross-browser issues.  
-// We suggest reviewing the code (in lib/game-shim.js) before enabling it.
 //
 define(function(require) {
+
+  // Toji's game-shim module is included with this template; if you wish to use
+  // it, uncomment the require() line below.
+  //
+  // The reason that this isn't done by default is that the replacement
+  // versions of the various APIs that it includes could potentially cause
+  // complications or performance issues with other libraries that use these APIs
+  // and may have their own strategies for dealing with cross-browser issues.  
+  // We suggest reviewing the code (in lib/game-shim.js) before enabling it.
+  //
+  //require('game-shim');
   
 });
